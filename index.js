d8262c78f21caba07b9dabf97a1d9a8ea32f7fad
picoloman6 <a2328b@naver.com>
2024-01-02 20:25:12 +0900

    1일차 마무리

--- index.js
+++ index.js
@@ -91,44 +91,15 @@ const makeMovieCard = data => {
 
 // 페이지 시작 후 이벤트
 document.addEventListener('DOMContentLoaded', async () => {
-  //   try {
-  //     const data = await getData();
+  try {
+    const data = await getData();
 
-  //     data.forEach(v => {
-  //       makeMovieCard(v);
-  //     });
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  //   const data = [];
-
-  const data = [];
-  for (let i = 0; i < 20; i++) {
-    if (i % 2 === 0) {
-      data.push({
-        id: 1,
-        title: 'The Shawshank Redemption',
-        overview:
-          'Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.',
-        posterUrl:
-          'https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg',
-        rating: 8.777
-      });
-    } else {
-      data.push({
-        id: 2,
-        title: 'The Lord of the Rings: The Return of the King',
-        overview:
-          'Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.',
-        posterUrl:
-          'https://image.tmdb.org/t/p/w500/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg',
-        rating: 9.111
-      });
-    }
+    data.forEach(v => {
+      makeMovieCard(v);
+    });
+  } catch (e) {
+    console.log(e);
   }
-  data.forEach(v => {
-    makeMovieCard(v);
-  });
 });
 
 // 제목 검색
