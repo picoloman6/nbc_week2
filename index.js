4f9ca20d736315f897169e705bb26fa3454043f8
picoloman6 <a2328b@naver.com>
2024-01-02 11:32:10 +0900

    header 생성

--- index.js
+++ index.js
@@ -25,6 +25,17 @@ const getData = async () => {
 };
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const data = await getData();
+  //   const data = await getData();
+  const data = [];
+
+  for (let i = 0; i < 20; i++) {
+    data.push({
+      title: "The Shawshank Redemption",
+      overview:
+        "Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.",
+      poster:
+        '"https://image.tmdb.org/t/p/w500//q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg"',
+    });
+  }
   console.log(data);
 });
