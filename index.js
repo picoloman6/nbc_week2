3319172b5c3f2455397335a26092e8d81dfe8d56
picoloman6 <a2328b@naver.com>
2024-01-02 12:31:26 +0900

    movie-title 슬라이드 적용용

--- index.js
+++ index.js
@@ -40,6 +40,10 @@ const makeMovieCard = data => {
   $title.className = 'movie-card-title';
   $content.className = 'movie-card-content';
 
+  if (title.length > 24) {
+    $title.classList.add('slide-text');
+  }
+
   $image.src = `${poster}`;
   $image.alt = 'no photo';
   $title.textContent = title;
@@ -52,17 +56,28 @@ const makeMovieCard = data => {
 };
 
 document.addEventListener('DOMContentLoaded', async () => {
-  const data = await getData();
-  //   const data = [];
+  //   const data = await getData();
+  const data = [];
 
-  //   for (let i = 0; i < 20; i++) {
-  //     data.push({
-  //       title: 'The Shawshank Redemption',
-  //       overview:
-  //         'Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.',
-  //       poster: 'https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg'
-  //     });
-  //   }
+  for (let i = 0; i < 20; i++) {
+    if (i % 2 === 0) {
+      data.push({
+        title: 'The Shawshank Redemption',
+        overview:
+          'Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.',
+        poster:
+          'https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg'
+      });
+    } else {
+      data.push({
+        title: 'The Lord of the Rings: The Return of the King',
+        overview:
+          'Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.',
+        poster:
+          'https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg'
+      });
+    }
+  }
 
   data.forEach(v => {
     makeMovieCard(v);
