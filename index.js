a5ce071a432fac399c36ed405769f134c68f4ba9
picoloman6 <a2328b@naver.com>
2024-01-02 17:13:46 +0900

    페이지 변경 구현

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
 const $main = document.querySelector('main');
 const $searchInput = document.querySelector('.search-input');
 const $searchButton = document.querySelector('.search-button');
+const $pageLabel = document.querySelector('.page-label');
+const $pageInput = document.querySelector('.page-input');
+const $pageButton = document.querySelector('.page-button');
 
 // 전역 상태
 let page = 1;
+let totalPages = 500;
+const cache = {};
 
 // 영화 정보 가져오기
 const getData = async () => {
@@ -27,6 +32,9 @@ const getData = async () => {
       posterUrl: `https://image.tmdb.org/t/p/w500${v['poster_path']}`,
       overview: v['overview']
     }));
+    totalPages = data['total_pages'];
+    $pageLabel.textContent = `페이지 변경(최대 ${totalPages})`;
+    cache[page] = results;
     return results;
   } catch (e) {
     console.log(e);
@@ -54,7 +62,7 @@ const makeMovieCard = data => {
   }
 
   $title.textContent = title;
-  $rating.textContent = `Average Rating : ${rating}`;
+  $rating.textContent = `Average Rating : ${String(rating).padEnd(5, '0')}`;
   $image.src = posterUrl;
   $image.alt = 'no photo';
   $overview.textContent = overview;
@@ -69,37 +77,42 @@ const makeMovieCard = data => {
 
 // 페이지 시작 후 이벤트
 document.addEventListener('DOMContentLoaded', async () => {
-  //   const data = await getData();
-  const data = [];
+  try {
+    const data = await getData();
 
-  for (let i = 0; i < 20; i++) {
-    if (i % 2 === 0) {
-      data.push({
-        title: 'The Shawshank Redemption',
-        overview:
-          'Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.',
-        posterUrl:
-          'https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg',
-        rating: 8.777
-      });
-    } else {
-      data.push({
-        title: 'The Lord of the Rings: The Return of the King',
-        overview:
-          'Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.',
-        posterUrl:
-          'https://image.tmdb.org/t/p/w500/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg',
-        rating: 9.111
-      });
-    }
+    data.forEach(v => {
+      makeMovieCard(v);
+    });
+  } catch (e) {
+    console.log(e);
   }
 
-  data.forEach(v => {
-    makeMovieCard(v);
-  });
+  //   const data = [];
+
+  //   for (let i = 0; i < 20; i++) {
+  //     if (i % 2 === 0) {
+  //       data.push({
+  //         title: 'The Shawshank Redemption',
+  //         overview:
+  //           'Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.',
+  //         posterUrl:
+  //           'https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg',
+  //         rating: 8.777
+  //       });
+  //     } else {
+  //       data.push({
+  //         title: 'The Lord of the Rings: The Return of the King',
+  //         overview:
+  //           'Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.',
+  //         posterUrl:
+  //           'https://image.tmdb.org/t/p/w500/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg',
+  //         rating: 9.111
+  //       });
+  //     }
+  //   }
 });
 
-// 검색
+// 제목 검색
 $searchButton.addEventListener('click', e => {
   const text = $searchInput.value.toLowerCase();
   const movieCards = document.querySelectorAll('.movie-card');
@@ -126,6 +139,44 @@ $searchButton.addEventListener('click', e => {
     } else {
       v.classList.remove('hidden');
     }
+  });
+
+  errMsg.classList.add('hidden');
+});
+
+// 페이지 변경
+$pageButton.addEventListener('click', async e => {
+  let data;
+  const value = $pageInput.value;
+  const num = value * 1;
+  const errMsg = document.querySelector('.page-err-msg');
+
+  e.preventDefault();
+
+  if (!/^[0-9]{1,3}$/g.test(value) || num < 1 || num > totalPages) {
+    errMsg.textContent = `1 ~ ${totalPages} 사이를 입력하세요.`;
+    errMsg.classList.remove('hidden');
+    return;
+  }
+
+  // 데이터 캐싱
+  page = num;
+  if (cache[page]) {
+    data = cache[page];
+  } else {
+    try {
+      data = await getData();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  while ($main.firstChild) {
+    $main.removeChild($main.firstChild);
+  }
+
+  data.forEach(v => {
+    makeMovieCard(v);
   });
 
   errMsg.classList.add('hidden');
