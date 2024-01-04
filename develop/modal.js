e57294a0d8e3b1d4401bf51324400a9e9a5cdea0
picoloman6 <a2328b@naver.com>
2024-01-04 15:19:56 +0900

    모달 스타일링 완성

--- develop/modal.js
+++ develop/modal.js
@@ -3,20 +3,32 @@ export const InfoModal = (movie, $body) => {
 
   const $wrapper = document.createElement('div');
   const $modal = document.createElement('div');
+  const $header = document.createElement('div');
   const $title = document.createElement('span');
-  const $content = document.createElement('span');
+  const $content = document.createElement('div');
+  const $id = document.createElement('span');
+  const $rating = document.createElement('span');
   const $button = document.createElement('button');
 
   $wrapper.className = 'info-modal-wrapper';
   $modal.className = 'info-modal-window';
-  $title.className = 'info-modal-title';
+  $header.className = 'info-modal-header';
   $content.className = 'info-modal-content';
+  $button.className = 'info-modal-button';
+
+  if (title.length > 20) {
+    $title.classList.add('slide-text');
+  }
 
   $title.textContent = title;
-  $content.textContent = `ID : ${id} 평점 : ${rating}`;
+  $id.textContent = `id : ${id}`;
+  $rating.textContent = `평점 : ${rating}`;
   $button.textContent = '닫기';
 
-  $modal.appendChild($title);
+  $header.append($title);
+  $content.appendChild($id);
+  $content.appendChild($rating);
+  $modal.appendChild($header);
   $modal.appendChild($content);
   $modal.appendChild($button);
   $wrapper.appendChild($modal);
